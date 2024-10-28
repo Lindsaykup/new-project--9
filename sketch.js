5e8a5deac0ae0b2122a5140e702f99a989bd720d
Lindsay Kuperman <LMK22E@FSU.EDU>
2024-10-27 23:51:44 -0400

    players setup

---
+++ sketch.js
@@ -0,0 +1,26 @@
+let player;
+let stars;
+let score = 0;
+
+function setup() {
+  createCanvas(800, 600);
+
+  // Player setup
+  player = new Sprite(width / 2, height - 50, 100, 20);
+  player.color = color("blue");
+
+  // Stars setup
+  stars = new Group();
+}
+
+
+
+
+
+
+
+
+
+
+
+
