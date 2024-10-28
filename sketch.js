e1e8f8cb40a23d0b36d79e1d82d8390e5b799315
Lindsay Kuperman <LMK22E@FSU.EDU>
2024-10-27 23:55:00 -0400

    score and bar work

--- sketch.js
+++ sketch.js
@@ -14,6 +14,48 @@ function setup() {
 }
 
 
+function draw() {
+	background(200);
+  
+	// Player movement
+	if (keyIsDown(LEFT_ARROW)) player.x -= 5;
+	if (keyIsDown(RIGHT_ARROW)) player.x += 5;
+  
+	// Constrain player within canvas
+	player.x = constrain(player.x, 0, width);
+	// Create falling stars
+	if (frameCount % 30 === 0) {
+		let star = new Sprite(random(width), 0, 20, 20);
+		star.color = color("yellow");
+		star.speed = 5;
+		stars.add(star);
+	  }
+	
+	  // Check if player catches stars
+	  player.overlap(stars, (collector, collected) => {
+		score++;
+		collected.remove();
+	  });
+
+	  // Remove stars that reach the bottom
+  stars.forEach(star => {
+    if (star.y > height) {
+      star.remove();
+      score--;
+    }
+  });
+
+  // Display score
+  textSize(24);
+  fill(0);
+  text(`Score: ${score}`, 10, 30);
+}
+	  
+
+	
+  
+
+
 
 
 
